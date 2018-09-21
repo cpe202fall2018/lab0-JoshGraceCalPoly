0824951ba7dbbeb494c887106909d36b2982f042
Josh Grace <poshamazing@gmail.com>
2018-09-21 12:16:26 -0700

    documented code

--- planets.py
+++ planets.py
@@ -1,8 +1,9 @@
+
+# converts a weight on earth to the weight on Mars/ Jupiter and prints that weight
 def weight_on_planets():
-    weight = float( input() )
-    marsWeight = weight * .38
-    jupiterWeight = weight * 2.34
-    print("What do you weigh on earth? \nOn Mars you would weigh " + str(marsWeight) + " pounds.\nOn Jupiter you would weigh " + str(jupiterWeight) + " pounds.")
-    # print( "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds.")
+    weight = float( input() ) # read the inputed weight and convert it to a float 
+    marsWeight = weight * .38 # convert the earth weight to weight on Mars
+    jupiterWeight = weight * 2.34 # convert the earth weight to the weight on Jupiter
+    print("What do you weigh on earth? \nOn Mars you would weigh " + str(marsWeight) + " pounds.\nOn Jupiter you would weigh " + str(jupiterWeight) + " pounds.") # print the return message with the calculated weights
 if __name__ == "__main__":
     weight_on_planets()
--- planets_tests.py
+++ planets_tests.py
@@ -4,22 +4,22 @@ from planets import *
 
 class Test_planets(unittest.TestCase):
 
+    # run a test to confirm the result with a normal weight
     def test_01(self):
-        sys.stdin = io.StringIO("136")
-        sys.stdout = student_output = io.StringIO()        
-        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
-        weight_on_planets()
-        # print(student_output.getvalue().strip())
-        self.assertEqual(expected_out, student_output.getvalue().strip())
+        sys.stdin = io.StringIO("136") # Pass the weight to the input of the method
+        sys.stdout = student_output = io.StringIO() # Read the print statement from the method
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds." # stage the expected answer
+        weight_on_planets() # run weight on planets to calculate the weight on Mars/ Jupiter
+        self.assertEqual(expected_out, student_output.getvalue().strip()) # check if the return value of weight_on_planets is what we expected
 
+    # run a second test to confirm the result with a smaller value
     def test_02(self):
         sys.stdin = io.StringIO("1")
         sys.stdout = student_output = io.StringIO()        
         expected_out = "What do you weigh on earth? \nOn Mars you would weigh 0.38 pounds.\nOn Jupiter you would weigh 2.34 pounds."
         weight_on_planets()
-        # print(student_output.getvalue().strip())
         self.assertEqual(expected_out, student_output.getvalue().strip())
 
-
+# run the main method
 if __name__ == "__main__":
         unittest.main()
