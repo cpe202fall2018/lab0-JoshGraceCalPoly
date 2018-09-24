dd0522bb49420f7cbadad03b3857305503bfc6ee
Josh Grace <poshamazing@gmail.com>
2018-09-24 07:41:19 -0700

    removed unneeded casting to string

--- planets.py
+++ planets.py
@@ -4,6 +4,6 @@ def weight_on_planets():
     weight = float( input() ) # read the inputed weight and convert it to a float 
     marsWeight = weight * .38 # convert the earth weight to weight on Mars
     jupiterWeight = weight * 2.34 # convert the earth weight to the weight on Jupiter
-    print("What do you weigh on earth? \nOn Mars you would weigh " + str(marsWeight) + " pounds.\nOn Jupiter you would weigh " + str(jupiterWeight) + " pounds.") # print the return message with the calculated weights
+    print("What do you weigh on earth? \nOn Mars you would weigh", marsWeight, "pounds.\nOn Jupiter you would weigh", jupiterWeight, "pounds.") # print the return message with the calculated weights
 if __name__ == "__main__":
     weight_on_planets()
